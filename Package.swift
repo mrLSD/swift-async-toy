85c2ffe8f57d87c0de93c9d9d5b46008c7be1114
Evgeny Ukhanov <evgeny@aurora.dev>
2023-12-16 21:14:26 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "async2",
+    name: "async1",
+    platforms: [
+        .macOS(.v14)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
@@ -12,10 +15,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "async2",
+            name: "async1",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
+            ],
+            path: "async1"
         ),
     ]
 )
